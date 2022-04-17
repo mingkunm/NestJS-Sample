fc7324707ddc31bea64f02a5ffd8bd1d3903eb98
Mingkun Ma <mmk.ee.911@gmail.com>
2022-04-16 22:25:13 -0700

    feat: Add first route wildcards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,7 +6,12 @@ export class CatsController {
     return 'This action adds a new cat';
   }
 
-  @Get()
+  /*
+    The 'ab*cd' route path will match abcd, ab_cd, abecd, and so on. 
+    The characters ?, +, *, and () may be used in a route path, and are subsets of their regular expression counterparts. 
+    The hyphen ( -) and the dot (.) are interpreted literally by string-based paths.
+  */
+  @Get('ab*cd')
   findAll(): string {
     return 'This action returns all cats';
   }
